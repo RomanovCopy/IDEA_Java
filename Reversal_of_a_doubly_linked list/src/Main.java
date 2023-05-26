fb95b56044952c7de7457207ab496039ccb489f9
Сергей Романов <RomanovCopy@gmail.com>
2023-05-26 15:38:46 +0300

    fix

--- Reversal_of_a_doubly_linked list/src/Main.java
+++ Reversal_of_a_doubly_linked list/src/Main.java
@@ -1,17 +1,57 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
+
+import java.util.LinkedList;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LinkedList<Integer>myList=null;
+        myList=createList(10);
+        printList(myList);
+        reverse(myList.getFirst());
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void reverse(Node head) {
+        Node current = head;
+        Node temp = null;
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        if (temp != null) {
+            head = temp.prev;
+        }
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static LinkedList<Integer> createList(int size){
+        LinkedList<Integer> list=new LinkedList<>();
+        for(int i=0;i<size;i++){
+            add(list,i);
+        }
+        return list;
+    }
+
+    public static void add(LinkedList<Integer> list, int value) {
+        Node head=list.get(0);
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+            newNode.setPrev(current);
+        }
+    }
+
+    public static void printList(LinkedList<Integer> list){
+        for(int val : list)
+           System.out.print(node);
         }
     }
 }
