fd0f57ca05e6b785094ebe43745953dc8cd19836
RomanovCopy <RomanovCopy@gmail.com>
2023-05-26 20:19:46 +0300

    end

--- Reversal_of_a_doubly_linked list/src/Main.java
+++ Reversal_of_a_doubly_linked list/src/Main.java
@@ -1,57 +1,68 @@
 
+//Необходимо реализовать метод разворота связного списка (двухсвязного или односвязного на выбор).
 
-
-import java.util.LinkedList;
+//В данном классе мы генерируем двусвязный список, выводим его в консоль, реверсируем,
+//и снова выводим в консоль уже в реверсированном виде
 
 public class Main {
 
+    static private Node head;//начальный элемент списка
+
     public static void main(String[] args) {
-        LinkedList<Integer>myList=null;
-        myList=createList(10);
-        printList(myList);
-        reverse(myList.getFirst());
+        //генерируем список
+        createList(10);
+        System.out.println("Список до реверсирования :");
+        //выводим список в начальном виде
+        printList();
+        //реверсируем список
+        reverse();
+        System.out.println("Список после реверсирования :");
+        //выводим список после реверсирования
+        printList();
     }
 
-    public static void reverse(Node head) {
+    //реверсирование списка
+    public static void reverse() {
         Node current = head;
         Node temp = null;
         while (current != null) {
-            temp = current.prev;
-            current.prev = current.next;
-            current.next = temp;
-            current = current.prev;
+            temp = current.getPrev();
+            current.setPrev(current.getNext());
+            current.setNext(temp);
+            current = current.getPrev();
         }
         if (temp != null) {
-            head = temp.prev;
+            head = temp.getPrev();
         }
     }
 
-    public static LinkedList<Integer> createList(int size){
-        LinkedList<Integer> list=new LinkedList<>();
-        for(int i=0;i<size;i++){
-            add(list,i);
-        }
-        return list;
-    }
-
-    public static void add(LinkedList<Integer> list, int value) {
-        Node head=list.get(0);
-        Node newNode = new Node(value);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
+    //генерация списка из заданного количества элементов
+    public static void createList(int size){
+       Node newNode=null;
+        Node current=null;
+        for(int i=0 ; i<size; i++){
+            newNode=new Node(i);
+            if(head==null){
+                head=newNode;
             }
-            current.setNext(newNode);
-            newNode.setPrev(current);
+            else {
+                current = head;
+                while (current.getNext() != null) {
+                    current = current.getNext();
+                }
+                current.setNext(newNode);
+                newNode.setPrev(current);
+            }
         }
     }
 
-    public static void printList(LinkedList<Integer> list){
-        for(int val : list)
-           System.out.print(node);
+    //вывод списка
+    public static void printList(){
+        Node current=head;
+        while (current!=null){
+            System.out.print(current.getValue()+" ");
+            current=current.getNext();
+        }
+        System.out.println();
         }
     }
-}
