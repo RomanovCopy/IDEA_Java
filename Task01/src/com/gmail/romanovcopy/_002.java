f0d820c0b5d812ac0f609e5585c8080eff3435f6
RomanovCopy <RomanovCopy@gmail.com>
2023-06-04 11:49:41 +0300

    fix

--- Task01/src/com/gmail/romanovcopy/_002.java
+++ Task01/src/com/gmail/romanovcopy/_002.java
@@ -1,10 +1,47 @@
 package com.gmail.romanovcopy;
 
+import java.util.Random;
+
+/*Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.*/
+
+
+
 public class _002 {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Автоматически созданная заглушка метода
-
+		
+		var array=createArray(100);
+		System.out.print(maxTrue(array));
+		
 	}
+	
+	private static boolean[] createArray(Integer length) {
+		Random rand=new Random();
+		boolean[]list =new boolean[length];
+		for(int i=0;i<length;i++) {
+			list[i]=rand.nextInt(2)==1;
+		}
+		return list;
+	}
+	
+	private static int maxTrue(boolean[]array) {
+		int max=0;
+		int count=0;
+		for(boolean val:array) {
+			if(val) {
+				count++;
+			}
+			else {
+				if(count>max) {
+					max=count;
+				}
+				count=0;
+			}
+		}
+		return max;
+	}
+	
 
 }
