ed01441bf95c7b4e2aab5fdbbea1a4cd5bf6e7bb
RomanovCopy <RomanovCopy@gmail.com>
2023-06-05 22:05:42 +0300

    fix

---
+++ MyProject/src/_010.java
@@ -0,0 +1,36 @@
+
+/*Задано уравнение вида q + w = e, q, w, e >= 0.
+Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
+Требуется восстановить выражение до верного равенства.
+Знаки вопроса - одинаковые цифры.
+Предложить хотя бы одно решение или сообщить, что его нет.*/
+
+public class _010 {
+
+    public _010(){
+
+    }
+
+    //получение уравнения
+    private String getEquation(){
+        var value="";
+
+        return value;
+    }
+
+    private String equationNormalization(String equation){
+        var value="";
+
+        return value;
+    }
+
+    //разбиение уравнения на составные части
+    private String[] gettingTheComponentsOfAnEquation(String equation){
+        String[]array=new String[3];
+
+        return array;
+    }
+
+
+
+}
