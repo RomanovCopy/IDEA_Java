8096c8160c225f72a829c8f5d4e41be4cc1a7da1
Сергей Романов <RomanovCopy@gmail.com>
2023-06-07 11:03:08 +0300

    fix

--- MyProject/src/_010.java
+++ MyProject/src/_010.java
@@ -16,15 +16,16 @@ public class _010 {
     String equation;//полное выражение
     String leftPart;//выражение до знака =
     String rightPart;//выражение после знака =
-    int unknown;//колличество неизвестных разрядов(вместо цифры - '?')
+    ArrayList<Integer>indexes;//позиции неизвестных элементов
 
     
     public _010(){
         operations=new Character []{'+', '-', '*', '/', '=' };
+        indexes=new ArrayList<>();
         System.out.print("Введи выражение: ");
         equation = getEquation();
-        if(leftPart.length()>0 && rightPart.length()>0){
-            var list = searchForOptions(leftPart, rightPart, unknown);
+        if(leftPart.length()>0 && rightPart.length()>0 && indexes.size()>0){
+            var list = searchForOptions(leftPart, rightPart);
             if(list.size()>0){
                 for(var eq:list){
                     System.out.println(eq);
@@ -45,13 +46,31 @@ public class _010 {
         var array=value.split("[=]");
         leftPart=array[0];
         rightPart=array[1];
-        for(int i=0; i<value.length();i++) unknown+=value.charAt(i)=='?'?1:0;
+        for(int i=0; i<value.length();i++){
+            if(value.charAt(i)=='?'){
+                indexes.add(i);
+            }
+        }
         return value;
     }
 
-    private ArrayList<String>searchForOptions(String left, String right, int number){
+    private ArrayList<String>searchForOptions(String left, String right){
         var list=new ArrayList<String>();
-
+        int digits=indexes.size();
+        var array=equation.toCharArray();
+        for(int index:indexes){
+            array[index]='0';
+        }
+        for(int i=1;i<Math.pow(10, digits);i++){
+            int value=(int)Math.pow(10, digits)+i;
+            for(int index:indexes){
+                int temp=value%10;
+                var str=Integer.toString(temp);
+                array[index]=str.toCharArray()[0];
+                value/=10;
+            }
+            
+        }
         return list;
     }
 }
