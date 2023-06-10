4e52698861399e4adbfebdb491836596497ad40c
RomanovCopy <RomanovCopy@gmail.com>
2023-06-10 17:05:34 +0300

    fix

--- MyProject/src/Task_01.java
+++ MyProject/src/Task_01.java
@@ -1,4 +1,3 @@
-import netscape.javascript.JSObject;
 
 
 public class Task_01 {
@@ -16,38 +15,11 @@ public class Task_01 {
      * city=Moscow
      */
 
-     String input_str = "{name:Ivanov,country:Russia,city:Moscow,age:null}";
 
     public Task_01() {
-        StringBuilder sb = new StringBuilder("select * from students WHERE ");
-        JSObject input = new JSObject(input_str);
-        String name = input.optString("name");
-        String country = input.optString("country");
-        String city = input.optString("city");
-        String age = input.optString("age");
 
-        if (name != null) {
-            sb.append("name=").append(name).append(" AND ");
-        }
-        if (country != null) {
-            sb.append("country=").append(country).append(" AND ");
-        }
-        if (city != null) {
-            sb.append("city=").append(city).append(" AND ");
-        }
-        if (age != null && !age.equals("null")) {
-            sb.append("age=").append(age).append(" AND ");
-        }
-
-        // remove the last "AND"
-        if (sb.toString().endsWith("AND ")) {
-            sb.delete(sb.length() - 5, sb.length());
-        }
-
-        String sqlQuery = sb.toString(); // select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow.
 
     }
 
-    private String 
 
 }
--- MyProject/src/Task_02.java
+++ MyProject/src/Task_02.java
@@ -1,3 +1,45 @@
+/*Реализуйте алгоритм сортировки пузырьком числового массива, 
+результат после каждой итерации запишите в лог-файл */
+
+import java.io.FileWriter;
+import java.security.cert.TrustAnchor;
+import java.util.Random;
+
 public class Task_02 {
-    
+
+    private FileWriter fWriter;
+
+    int[]myArray;
+
+    public Task_02() {
+
+        fWriter = new FileWriter("myLog.txt", true);
+        myArray = createArray(50);
+
+    }
+
+    private void logging(int iteration, int[] array) {
+
+        fWriter.write("After iteration " + (iteration + 1) + ": ");
+        for (int k = 0; k < array.length; k++) {
+            fWriter.write(array[k] + " ");
+        }
+    }
+
+    /**
+     * создание массива заданного размера с
+     * рандомными значениями
+     * 
+     * @param number размер массива
+     * @return запрашиваемый массив
+     */
+    private int[] createArray(int number) {
+        var array = new int[number];
+        var random = new Random(100);
+        for (int i = 0; i < number; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+
 }
