15368ab1a5bb9b25ae87e4f84ede794123fdc7c4
RomanovCopy <RomanovCopy@gmail.com>
2023-06-10 22:01:01 +0300

    fix

--- MyProject/src/Programm.java
+++ MyProject/src/Programm.java
@@ -3,7 +3,8 @@ public class Programm {
     public static void main(String[] args) {
         //new Task_01();
         //new Task_02();
-        new Task_03();
+        //new Task_03();
+        new Task_04();
     }
     
 }
