1da7bc3d453c01e16d23095c32688b0ded3fb2f4
RomanovCopy <RomanovCopy@gmail.com>
2023-06-17 11:39:59 +0300

    fix

--- MyProject/src/Program.java
+++ MyProject/src/Program.java
@@ -1,10 +1,7 @@
 public class Program {
 
     public static void main(String[] args) {
-        //new _010(); 
-        //new _020();
-        //new _030();
-        new _040();
+        
     }
 
 }
